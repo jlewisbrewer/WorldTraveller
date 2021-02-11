00561d8051467c933a50d09d57464c14abaa526b
Jason Brewer <jlewisbrewer@gmail.com>
2021-02-10 19:19:00 -0800

    Gutted files to create a static site

--- src/test/java/com/worldtraveller/WebApiApplicationTests.java
+++ src/test/java/com/worldtraveller/WebApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.worldtraveller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class WebApiApplicationTests {
+// @SpringBootTest
+// class WebApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	// @Test
+// 	// void contextLoads() {
+// 	// }
 
-}
+// }
